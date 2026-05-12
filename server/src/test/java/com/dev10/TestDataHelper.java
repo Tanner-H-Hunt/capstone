de0cf5ca7fb166dd75861eaa5296e0daab3dae37
Tanner Hunt <thunt@dev-10.com>
2026-05-12 14:44:14 -0600

    added test data to spring for testing file services

--- server/src/test/java/com/dev10/TestDataHelper.java
+++ server/src/test/java/com/dev10/TestDataHelper.java
@@ -1,5 +1,7 @@
 package com.dev10;
 
+import com.dev10.models.FileSystemObject;
+import com.dev10.models.FileSystemObjectType;
 import com.dev10.models.User;
 
 import java.util.List;
@@ -30,5 +32,71 @@ public class TestDataHelper {
         user.setPassword("c");
 
         return user;
+    }
+
+    public static List<FileSystemObject> getFileSystemObjectsForUser1(){
+        FileSystemObject fso1 = new FileSystemObject();
+        fso1.setId(1);
+        fso1.setName("user1-directory");
+        fso1.setDocumentType(FileSystemObjectType.DIRECTORY);
+        fso1.setParentDirectoryId(0);
+
+        FileSystemObject fso2 = new FileSystemObject();
+        fso2.setId(1);
+        fso2.setName("user1-todo");
+        fso2.setDocumentType(FileSystemObjectType.TODO);
+        fso2.setParentDirectoryId(0);
+
+        FileSystemObject fso3 = new FileSystemObject();
+        fso3.setId(2);
+        fso3.setName("user1-uml");
+        fso3.setDocumentType(FileSystemObjectType.UML);
+        fso3.setParentDirectoryId(1);
+
+        FileSystemObject fso4 = new FileSystemObject();
+        fso4.setId(3);
+        fso4.setName("user1-note");
+        fso4.setDocumentType(FileSystemObjectType.NOTE);
+        fso4.setParentDirectoryId(0);
+
+        return List.of(
+                fso1,
+                fso2,
+                fso3,
+                fso4
+        );
+    }
+
+    public static List<FileSystemObject> getFileSystemObjectsForUser2(){
+        FileSystemObject fso1 = new FileSystemObject();
+        fso1.setId(2);
+        fso1.setName("user2-directory");
+        fso1.setDocumentType(FileSystemObjectType.DIRECTORY);
+        fso1.setParentDirectoryId(0);
+
+        FileSystemObject fso2 = new FileSystemObject();
+        fso2.setId(2);
+        fso2.setName("user2-todo");
+        fso2.setDocumentType(FileSystemObjectType.TODO);
+        fso2.setParentDirectoryId(0);
+
+        FileSystemObject fso3 = new FileSystemObject();
+        fso3.setId(3);
+        fso3.setName("user2-uml");
+        fso3.setDocumentType(FileSystemObjectType.UML);
+        fso3.setParentDirectoryId(1);
+
+        FileSystemObject fso4 = new FileSystemObject();
+        fso4.setId(4);
+        fso4.setName("user2-note");
+        fso4.setDocumentType(FileSystemObjectType.NOTE);
+        fso4.setParentDirectoryId(0);
+
+        return List.of(
+                fso1,
+                fso2,
+                fso3,
+                fso4
+        );
     }
 }
