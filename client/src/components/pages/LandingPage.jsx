ad0fd3e6699a85207b41ebea24e8033b5057ba10
Tanner Hunt <thunt@dev-10.com>
2026-05-12 11:28:59 -0600

    added summary to landing page and placeholder image

--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { div } from "three/tsl";
+import Placeholder from '../../assets/placeholder.png'
 
 function LandingPage(){
     return (
@@ -23,10 +24,25 @@ function LandingPage(){
                 </div>
                 <div className="col-2"></div>
             </div>
-            <div className="row">
-                <div className="col-6 ms-2">
-                    <p>lorem ipsum dolore sur gutra </p>
+
+            <div className="row py-2">
+                <div className="col-6 px-3">
+                    <p className="fs-4">Build software with context, not scattered documentation. Our platform gives development teams a unified workspace to create UML diagrams, manage tickets, and write technical design documents — all while connecting related pieces of information across your system architecture. Instead of treating diagrams, tasks, and documentation as isolated artifacts, users can create meaningful relationships between them, making it easier to trace features from initial design to implementation.</p>
+                    <button className="btn btn-primary">Register</button>
                 </div>
+                <div className="col-6 d-flex justify-content-center align-items-center">
+                    <img src={Placeholder} alt="" className="img-fluid"/>
+                </div>
+
+            </div>
+
+            <div className="row">
+                <div className="col-2"></div>
+                <div className="col-8">
+                    <h1 className="text-center">Features</h1>
+
+                </div>
+                <div className="col-2"></div>
             </div>
         </div>
         </>
