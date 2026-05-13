b84ec72c9495798c8f715ed90beff3d2c7d6c6bd
Tanner Hunt <thunt@dev-10.com>
2026-05-13 12:26:29 -0600

    DirectoryService getDirectory... methods

--- server/src/main/java/com/dev10/domain/DirectoryService.java
+++ server/src/main/java/com/dev10/domain/DirectoryService.java
@@ -1,7 +1,26 @@
 package com.dev10.domain;
 
+import com.dev10.models.Directory;
+import com.dev10.models.User;
+import com.dev10.repositories.DirectoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DirectoryService {
+
+    private final DirectoryRepository directoryRepository;
+
+    public DirectoryService(DirectoryRepository directoryRepository){
+        this.directoryRepository = directoryRepository;
+    }
+
+    public List<Directory> getRootDirectories(User user){
+        return directoryRepository.getRootDirectories(user);
+    }
+
+    public List<Directory> getDirectoriesInDirectory(int id){
+        return directoryRepository.getDirectoriesInDirectory(id);
+    }
 }
