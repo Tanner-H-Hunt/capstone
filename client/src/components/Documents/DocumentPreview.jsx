bd491c2f9e2f1361f1a53cf6eea7fc0583a7cf5d
Tanner Hunt <thunt@dev-10.com>
2026-05-17 15:58:48 -0600

    document titles can now be edited on the front end

--- client/src/components/Documents/DocumentPreview.jsx
+++ client/src/components/Documents/DocumentPreview.jsx
@@ -1,23 +1,63 @@
 import { useNavigate } from 'react-router';
 import Placeholder from '../../assets/placeholder.png'
+import { useState } from 'react';
+import ClickAwayListener from '@mui/material/ClickAwayListener'
 
 function DocumentPreview({ document }){
     const navigate = useNavigate();
+    const [editing, setEditing] = useState(false);
+    const [docName, setDocName] = useState(document.name);
 
     function redirect(){
         const id = document.id;
         navigate("/document/" + id);
     }
 
+    function onSubmit(){
+        event.preventDefault();
+        //TODO send edit to server, validate
+        setEditing(false);
+    }
+
+    function onDoubleClick(){
+        setEditing(!editing);
+    }
+
+    function handleChange(evt){
+        setDocName(evt.target.value);
+    }
+
+    function handleClickAway(){
+        if(editing){
+            console.log("clicked away");
+            setEditing(false);
+        }
+    }
+
     return(
         <div className="card">
             <button className="btn" onClick={() => redirect()}>
                 <img src={Placeholder} className="card-img-top p-2" alt=""/>
-                <div className="card-body px-0 py-1">
-                    <p className='fs-6 card-text text-start'>{document.name}</p>
-                </div>
             </button>
+
             <ul className='list-group list-group-flush'>
+                
+                <ClickAwayListener onClickAway={() => handleClickAway()}>
+                <li className='list-group-item' onDoubleClick={onDoubleClick}>
+                    {editing ? 
+                    <form onSubmit={onSubmit}>
+                        <input 
+                            id={document.id}
+                            className=''
+                            type="text" 
+                            onChange={handleChange}
+                            value={docName}
+                            name='documentName'
+                        />
+                    </form> : <p className='my-0'>{docName}</p>}
+                </li>
+                </ClickAwayListener>
+
                 <li className='list-group-item text-muted'>{document.documentType}</li>
                 <li className='list-group-item text-muted'>
                     <button className='btn btn-danger'>Delete</button>
